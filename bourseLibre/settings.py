65c46ad405a506337a67e0324c6c3f7bb5782d07
eloi.grau <eloi.grau@irstea.fr>
2019-02-27 17:42:31 +0100

    modifié :         bourseLibre/settings.py

--- bourseLibre/settings.py
+++ bourseLibre/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'a#_dnav(b21!_zq7qcllxducb=%t9tl+(#2pfl)ao&bkh088p*x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
