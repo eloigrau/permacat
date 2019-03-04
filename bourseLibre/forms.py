a5610fe7f0db1b43659d2191e83dd564f8517ff7
eloi.grau@gmail.com <eloi.grau@gmail.com>
2019-03-04 18:05:28 +0100

    db

--- bourseLibre/forms.py
+++ bourseLibre/forms.py
@@ -124,6 +124,8 @@ class ProducteurCreationForm(UserCreationForm):
         return user
 
 
+#class UserAdmin(AuthUserAdmin):
+#    add_form = ProducteurCreationForm
 
 class ProducteurChangeForm(UserChangeForm):
     """A form for updating users. Includes all the fields on
