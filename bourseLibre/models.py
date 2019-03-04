a0f0455f0a580294097cf1321101711d462ee4f4
eloigrau <eloi.grau@gmail.com>
2019-03-04 22:30:22 +0100

    Update models.py

--- bourseLibre/models.py
+++ bourseLibre/models.py
@@ -156,7 +156,7 @@ class Profil(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created and instance.is_superuser:
+    if created:
         adresse = Adresse.objects.create()
         Profil.objects.create(user=instance, adresse=adresse)
         Panier.objects.create(user=Profil.objects.get(user=instance))
