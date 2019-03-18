046e317d6848a9db7630a3aa14ef31b9d0517aef
eloi.grau@gmail.com <eloi.grau@gmail.com>
2019-03-18 15:21:29 +0100

    db

--- bourseLibre/settings.py
+++ bourseLibre/settings.py
@@ -135,7 +135,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bourseLibre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-locall  = True
+locall  = False
 if locall:
     DATABASES = {
        'default': {
