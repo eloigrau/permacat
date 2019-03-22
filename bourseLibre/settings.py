e5a71b92f161de79c14eb04198bf8360146f99f1
eloi.grau@gmail.com <eloi.grau@gmail.com>
2019-03-22 02:24:57 +0100

    db

--- bourseLibre/settings.py
+++ bourseLibre/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 #DEBUG_PROPAGATE_EXCEPTIONS = True
 
 if DEBUG:
