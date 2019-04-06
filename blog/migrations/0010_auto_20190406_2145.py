01462835fb05cca77aca419c1d952251afd63e73
marchelibre <marchelibre@gmx.fr>
2019-04-06 21:57:11 +0200

    fichier projet

--- blog/migrations/0010_auto_20190406_2145.py
+++ blog/migrations/0010_auto_20190406_2145.py
@@ -10,16 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='projet',
-            name='statut',
-            field=models.CharField(choices=[('prop', 'Proposition'), ('AGO', "Soumis à l'AGO"), ('vote', 'Soumis au vote'), ('accep', 'Accepté'), ('refus', 'Refusé')], default='prop', max_length=5, verbose_name='statut'),
-        ),
-        migrations.AddField(
-            model_name='projet',
-            name='categorie',
-            field=models.CharField(choices=[('Part', 'Participation à un évènement'), ('AGO', "Organisation d'une AGO"), ('Projlong', 'Projet a long terme'), ('Projcourt', 'Projet a court terme')], default='Part', max_length=10, verbose_name='categorie'),
-        ),
         migrations.RenameField(
             model_name='projet',
             old_name='fichier',
