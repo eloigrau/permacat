a2a5d7d39d30191f6e27f3e3282e326276b2f648
marchelibre <marchelibre@gmx.fr>
2019-04-10 14:16:08 +0200

    fichier projet

--- bourseLibre/settings.py
+++ bourseLibre/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 LOCALL = False
 try:
     SECRET_KEY = os.environ['SECRET_KEY']
