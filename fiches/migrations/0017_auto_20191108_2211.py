46c1fe1a02ffccbf278089798335f19d0d23c1f6
marchelibre <marchelibre@gmx.fr>
2019-11-08 22:15:31 +0100

    kit fiches

--- fiches/migrations/0017_auto_20191108_2211.py
+++ fiches/migrations/0017_auto_20191108_2211.py
@@ -3,6 +3,7 @@
 from django.db import migrations, models
 import uuid
 
+from fiches.models import Fiche, Atelier  #where app == tango_app_name
 
 class Migration(migrations.Migration):
 
@@ -10,15 +11,39 @@ class Migration(migrations.Migration):
         ('fiches', '0016_auto_20191108_2209'),
     ]
 
+    def gen_uuid(apps, schema_editor):
+        for row in Fiche.objects.all():
+            row.slug = uuid.uuid4()
+            row.save()
+
+        for row in Atelier.objects.all():
+            row.slug = uuid.uuid4()
+            row.save()
+
     operations = [
-        migrations.AddField(
-            model_name='atelier',
-            name='slug',
-            field=models.SlugField(default=uuid.uuid4, max_length=100, unique=True),
-        ),
         migrations.AddField(
             model_name='fiche',
             name='slug',
-            field=models.SlugField(default=uuid.uuid4, max_length=100, unique=True),
+            field=models.SlugField(default=uuid.uuid4),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='atelier',
+            name='slug',
+            field=models.SlugField(default=uuid.uuid4),
+            preserve_default=True,
+        ),
+
+        migrations.RunPython(gen_uuid),
+
+        migrations.AlterField(
+            model_name='fiche',
+            name='slug',
+            field=models.SlugField(max_length=100, unique=True),
+        ),
+        migrations.AlterField(
+            model_name='atelier',
+            name='slug',
+            field=models.SlugField(max_length=100, unique=True),
         ),
     ]
