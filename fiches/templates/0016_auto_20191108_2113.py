a2a5159e8836a0f34232e7534afa33f2def726bb
marchelibre <marchelibre@gmx.fr>
2019-11-08 22:11:21 +0100

    kit fiches

--- fiches/templates/0016_auto_20191108_2113.py
+++ fiches/templates/0016_auto_20191108_2113.py
@@ -36,13 +36,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='fiche',
             name='slug',
-            field=models.SlugField(max_length=100, default=uuid.uuid4),
+            field=models.SlugField(default=uuid.uuid4),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='atelier',
             name='slug',
-            field=models.SlugField(max_length=100, default=uuid.uuid4),
+            field=models.SlugField(default=uuid.uuid4),
             preserve_default=True,
         ),
 
