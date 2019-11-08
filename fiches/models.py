71c33bdf11ee65079fa3f11131ae455490b0a4d6
marchelibre <marchelibre@gmx.fr>
2019-11-08 22:39:46 +0100

    kit fiches

--- fiches/models.py
+++ fiches/models.py
@@ -46,7 +46,7 @@ class Fiche(models.Model):
         default='proposition', verbose_name="statut de la fiche")
     numero = models.PositiveIntegerField(blank=False, default=1)
     titre = models.CharField(max_length=120)
-    slug = models.SlugField(max_length=100)
+    #slug = models.SlugField(max_length=100)
     contenu = models.TextField(null=True, blank=True)
     objectif = models.TextField(null=True, blank=True)
     en_savoir_plus = models.TextField(null=True, blank=True,)
