909dfae302a2e59e6dc6349bd30384a2f54ad60f
marchelibre <marchelibre@gmx.fr>
2020-01-06 19:25:35 +0100

    kit fiches

--- cal/utils.py
+++ cal/utils.py
@@ -1,13 +1,12 @@
 # cal/utils.py
 
-from datetime import datetime, timedelta
-from calendar import LocaleHTMLCalendar
+from datetime import datetime
+from calendar import LocaleHTMLCalendar, LocaleTextCalendar, month_name
 from blog.models import Article, Projet
 from ateliers.models import Atelier
 from django.db.models import Q
-import random
 
-class Calendar(LocaleHTMLCalendar):
+class Calendar(LocaleTextCalendar):
     def __init__(self, year=None, month=None):
         self.year = year
         self.month = month
@@ -31,19 +30,24 @@ class Calendar(LocaleHTMLCalendar):
             d += "<div class=' event'> <a href='"+event.get_absolute_url() +"'>"+titre+'</a> </div>'
 
         now = datetime.now()
+        aujourdhui=0
         if now.year > self.year or (now.year == self.year and now.month > self.month) :
-            style = "style='background-color:darkgrey'"
-        elif now.year == self.year and now.month == self.month and now.day > day:
             style = "style='background-color:grey'"
+        elif now.year == self.year and now.month == self.month and now.day > day:
+            style = "style='background-color:#bbeebb'"
         elif now.year == self.year and now.month == self.month and now.day == day:
-            style = "style='background-color:#66ff66 datecourante'"
+            style = "style='background-color:#85e085; '"
+            aujourdhui=1
         else:
             style = "style='background-color:#ccffcc;'"
 
         if day != 0:
+            if aujourdhui == 1:
+                return "<td "+style+" class='day'><span class='datecourante'>"+str(day)+'</span>'+str(d)+'</td>'
+            else:
                 return "<td "+style+" class='day'><span class='date'>"+str(day)+'</span>'+str(d)+'</td>'
 
-        return "<td></td>"
+        return "<td class='other-month' style='background-color:lightgrey'></td>"
 
     # formats a week as a tr
     def formatweek(self, theweek, events_arti, events_proj, events_per_day_atel):
@@ -62,11 +66,17 @@ class Calendar(LocaleHTMLCalendar):
         events_arti = Article.objects.filter(start_time__year=self.year, start_time__month=self.month)
         events_proj = Projet.objects.filter(start_time__year=self.year, start_time__month=self.month)
         events_atel = Atelier.objects.filter(date_atelier__year=self.year, date_atelier__month=self.month)
-        cal = '<table border="0" cellpadding="0" cellspacing="0" class="calendar table-condensed" id="calendar">\n'
-        cal += self.formatmonthname(self.year, self.month, withyear=withyear)+'\n'
-        s = ''.join(self.formatweekday(i) for i in self.iterweekdays())
-        cal += '<tr class="weekdays">%s</tr>' % s+'\n'
+
+
+        cal = '<div class="row moiscal"><div class="col-sm-12 ">' + self.formatmonthname(self.year, self.month, withyear=withyear, width=15)+ '</div></div>\n'
+        cal += '<table  class=" table-condensed" id="calendar">\n'
+        #cal += self.formatmonthname(self.year, self.month, withyear=withyear)+'\n'
+
+        for i in self.iterweekdays():
+            cal += "<th class='weekdays'>"+ self.formatweekday(i, width=10) + '</th>'
+
         for week in self.monthdays2calendar(self.year, self.month):
             cal += self.formatweek(week, events_arti, events_proj, events_atel)+'\n'
         cal += '</table>\n'
+        print (cal)
         return cal
