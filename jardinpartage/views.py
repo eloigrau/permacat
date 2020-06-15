084c70a9e4c9f985c3fa93e1093774516548e86e
eloigra <eloi.grau@gmail.com>
2020-06-15 20:54:27 +0200

    Ajout partie jardins partages - suivis suiveurs inscription jardins partagés

--- jardinpartage/views.py
+++ jardinpartage/views.py
@@ -252,7 +252,7 @@ def articles_suivis(request, slug):
 def articles_suiveurs(request):
     suivi, created = Suivis.objects.get_or_create(nom_suivi = 'articles_jardin')
     inscrits = Profil.objects.filter(is_jardinpartage=True).order_by('username')
-    suiveurs = followers(suivi).sort()
+    suiveurs = followers(suivi)
     return render(request, 'jardinpartage/articles_suivis.html', {'suiveurs': suiveurs, 'inscrits':inscrits})
 
 
