581207117bce8a877858ad81190f033576b2c8cf
eloigra <eloi.grau@gmail.com>
2020-09-18 01:13:37 +0200

    MAJ notifications permacat

--- bourseLibre/views_notifications.py
+++ bourseLibre/views_notifications.py
@@ -73,6 +73,7 @@ def getNotificationsParDate(request, limiter=True, orderBy="-timestamp"):
 
     actions = Action.objects.filter( \
             Q(verb='envoi_salon')| Q(verb__icontains='public')|Q(verb__icontains='Public')|
+            Q(verb='article_message') |Q(verb='projet_message') |
             Q(verb__startswith='fiche')|Q(verb__startswith='atelier')|
             Q(verb='envoi_salon_prive', description="a envoyé un message privé à " + request.user.username)|
             Q(verb__startswith='inscription'))
