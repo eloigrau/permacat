9133e3183bec60fd08d2296b0fb421eac9a8beb6
eloigra <eloi.grau@gmail.com>
2020-09-29 15:00:23 +0200

    MAJ notifications permacat

--- jardinpartage/views.py
+++ jardinpartage/views.py
@@ -157,7 +157,7 @@ class ListeArticles(UserPassesTestMixin, ListView):
         context['list_archive'] = self.qs.filter(estArchive=True)
         # context['producteur_list'] = Profil.objects.values_list('username', flat=True).distinct()
         context['auteur_list'] = Article.objects.order_by('auteur').values_list('auteur__username', flat=True).distinct()
-        cat= self.qs.objects.order_by('categorie').values_list('categorie', flat=True).distinct()
+        cat= self.qs.order_by('categorie').values_list('categorie', flat=True).distinct()
         context['categorie_list'] = [(x[0], x[1], Choix.get_couleur(x[0])) for x in Choix.type_annonce if x[0] in cat]
         context['typeFiltre'] = "aucun"
         context['suivis'], created = Suivis.objects.get_or_create(nom_suivi="articles_jardin")
