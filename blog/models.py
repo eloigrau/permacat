942e10116d082670f30f3c8216d05fb4b3766d1e
eloigra <eloi.grau@gmail.com>
2021-02-22 13:55:46 +0100

    heroku to perma;cat

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Choix():
     type_projet = ('Part','Participation à un évènement'), ('AGO',"Organisation d'une AGO"), ('Projlong','Projet a long terme'), ('Projcourt','Projet a court terme'), ('Projponct','Projet ponctuel'),
     type_annonce = ('Annonce','Annonce'), ('Administratif','Administratif'), ('Agenda','Agenda'),  ('Chantier','Chantier participatif'),\
                    ('Documentation','Documentation'), \
-                    ('Point', 'Point de vue'),  ('Recette', 'Recette'), \
+                    ('Point', 'Point de vue'),  ('Recette', 'Recette'), ('BonPlan','Bon Plan / achat groupé'), \
                      ('Divers','Divers'), #('Jardi','Jardi per tots'),
     type_annonce_projets = ('Altermarché', 'Altermarché'),  ('Ecovillage', 'Ecovillage'), \
                    ('Jardin', 'Jardins partagés'), #('KitPerma', 'Kit Perma Ecole'),
--- script_update.sh
+++ script_update.sh
@@ -5,5 +5,6 @@ git pull
 source /home/udjango/permacat/permacatenv/bin/activate
 sudo supervisorctl restart permacat_supervisor
 export DJANGO_SETTINGS_MODULE=bourseLibre.settings.production
+python manage.py migrate
 python manage.py runcrons
 
