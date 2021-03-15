871b1d97bf03c0061e6bec0c408d8a799b17d87e
eloigra <eloi.grau@gmail.com>
2021-03-15 14:28:34 +0100

    Changement interface

--- cal/utils.py
+++ cal/utils.py
@@ -62,7 +62,7 @@ class Calendar(LocaleTextCalendar):
                 d += "<div class='event'>  <a href='"+event.get_absolute_url() +"'><i class='fa fa-folder-open iconleft' ></i> "+titre+'</a> </div>'
         #for event in events_per_day_atel:
          #   titre = event.titre if len(event.titre)<40 else event.titre[:37] + "..."
-          #  d += "<div class='event'> <a href='"+event.get_absolute_url() +"'><i class='fa fa-wrench iconleft' ></i> "+titre+'</a> </div>'
+          #  d += "<div class='event'> <a href='"+event.get_absolute_url() +"'><i class='fa fa-dna iconleft' ></i> "+titre+'</a> </div>'
 
         for event in events_per_day_autre:
             if event.estPublic or (not request.user.is_anonymous and request.user.adherent_permacat):
