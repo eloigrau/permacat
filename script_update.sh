029821fd81bcb0efd42aca5da66876a986bea77d
eloigra <eloi.grau@gmail.com>
2021-03-15 13:13:54 +0100

    Changement interface

--- script_update.sh
+++ script_update.sh
@@ -3,5 +3,6 @@ cd /home/udjango/permacat
 git pull
 source /home/udjango/permacat/permacatenv/bin/activate
 python manage.py migrate --settings=bourseLibre.settings.production
+python manage.py collectstatic --noinput
 sudo supervisorctl restart permacat_supervisor
 
