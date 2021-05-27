c3a5b01c0aa6e7d3a0037ee2a3222e168e94d3ea
eloigra <eloi.grau@gmail.com>
2021-05-27 23:12:40 +0200

    jout photos

--- bourseLibre/urls.py
+++ bourseLibre/urls.py
@@ -164,10 +164,8 @@ urlpatterns += [
 ]
 
 from django.conf import settings
-from django.conf.urls.static import static
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
+    from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = h404
