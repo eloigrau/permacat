d3233742c0f88669ac2a206c633257394503a919
eloigra <eloi.grau@gmail.com>
2021-08-15 23:50:31 +0200

    votes

--- bourseLibre/views_notifications.py
+++ bourseLibre/views_notifications.py
@@ -575,7 +575,7 @@ def envoyerEmailstest():
 
 def voirDerniersArticlesVus(request):
     hit_count = Hit.objects.all().order_by('-created').distinct()[:50]
-    ht = {str(x.hitcount.content_object):[x.created, x.hitcount.content_object.get_absolute_url] for i, x in enumerate(hit_count) if x.hitcount.content_object}
+    ht = {str(x.hitcount.content_object):[x.created, x.hitcount.content_object.get_absolute_url, x.user] for i, x in enumerate(hit_count) if x.hitcount.content_object}
     ht = {x:y for i, (x, y) in enumerate(ht.items()) if i < 12}
     hit_count_perso = Hit.objects.filter(user=request.user.id).order_by('-created').distinct()[:50]
 
