d82635a8ca53d939e2aa0f22fcec1928d867cdd8
eloigra <eloi.grau@gmail.com>
2021-11-24 22:16:01 +0100

    citealtruiste

--- vote/admin.py
+++ vote/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from . import models
+from vote import models
 # Register your models here.
 
 admin.site.register(models.Vote)
 admin.site.register(models.Suffrage)
 admin.site.register(models.Question_majoritaire)
-admin.site.register(models.Proposition_m)
+#admin.site.register(models.Proposition_m)
 admin.site.register(models.Question_binaire)
 admin.site.register(models.ReponseQuestion_m)
 admin.site.register(models.ReponseQuestion_b)
