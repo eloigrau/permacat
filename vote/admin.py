02f2af18e42a929744c84641d9b93c273b435f98
eloigra <eloi.grau@gmail.com>
2021-11-25 22:32:59 +0100

    citealtruiste

--- vote/admin.py
+++ vote/admin.py
@@ -5,7 +5,7 @@ from vote import models
 admin.site.register(models.Vote)
 admin.site.register(models.Suffrage)
 admin.site.register(models.Question_majoritaire)
-#admin.site.register(models.Proposition_m)
+admin.site.register(models.Proposition_m)
 admin.site.register(models.Question_binaire)
 admin.site.register(models.ReponseQuestion_m)
 admin.site.register(models.ReponseQuestion_b)
