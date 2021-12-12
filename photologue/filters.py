c30b45c0853c6293815a84e15e6d542f6ae2655d
eloigra <eloi.grau@gmail.com>
2021-12-12 22:30:06 +0100

    citealtruiste

--- photologue/filters.py
+++ photologue/filters.py
@@ -8,13 +8,12 @@ class DocumentFilter(django_filters.FilterSet):
     asso = django_filters.ModelMultipleChoiceFilter(field_name='asso', queryset=Asso.objects.all().exclude(abreviation="jp"),
         widget=forms.CheckboxSelectMultiple)
     titre = django_filters.CharFilter(lookup_expr='icontains',)
-    contenu = django_filters.CharFilter(lookup_expr='icontains', )
     auteur = django_filters.ModelChoiceFilter(field_name='auteur', queryset=Profil.objects.all().extra(\
     select={'lower_name':'lower(username)'}).order_by('lower_name'), )
     #date_creation = django_filters.DateFromToRangeFilter(label="Date de création de l'article", widget=forms.DateInput(attrs={'class':"date", }))
     #start_time = django_filters.DateFromToRangeFilter(label="Date de l'evenement associé à l'article", widget=forms.DateInput(attrs={'class':"date", }))
     date_creation = django_filters.NumberFilter(
-        field_name='date_creation', method='get_past_n_days', label="Articles créés depuis X jours")
+        field_name='date_creation', method='get_past_n_days', label="Documents créés depuis X jours")
 
     def get_past_n_days(self, queryset, field_name, value):
         time_threshold = datetime.now() - timedelta(days=int(value))
@@ -24,7 +23,6 @@ class DocumentFilter(django_filters.FilterSet):
         model = Document
         fields = {
             'titre': ['icontains', ],
-            'contenu': ['icontains', ],
             'auteur': ['exact', ],
             "asso": ['exact', ],
         }
