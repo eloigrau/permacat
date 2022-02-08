17930f680d30b976b74c55691db55bd1b66cc571
eloigra <eloi.grau@gmail.com>
2022-02-08 10:40:15 +0100

    citealtruiste

--- blog/models.py
+++ blog/models.py
@@ -447,9 +447,9 @@ class AdresseArticle(models.Model):
 
     def __str__(self):
         if self.titre:
-            return str(self.titre) + " : " + str(self.adresse.get_adresse_str())
+            return str(self.titre) + " : " + str(self.adresse.get_adresse_str)
         else:
-            return str(self.adresse.get_adresse_str())
+            return str(self.adresse.get_adresse_str)
 
 
     @property
