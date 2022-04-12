01d747218fb031bf3045d98a664c7ebff66dcf55
eloigra <eloi.grau@gmail.com>
2022-04-12 21:31:26 +0200

    citealtruiste

--- agoratransition/forms.py
+++ agoratransition/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from.models import InscriptionExposant, Proposition, Message_agora
 from django.core.mail import send_mail
 from bourseLibre.settings.production import SERVER_EMAIL, LOCALL
-from envoi_mail import envoyerMailPermAgora
+from .envoi_mail import envoyerMailPermAgora
 #from django_summernote.widgets import SummernoteWidget
 
 LIST_EMAIL_SUIVI = ['eloi.grau@gmail.com', "permagora66@gmail.com", ]
