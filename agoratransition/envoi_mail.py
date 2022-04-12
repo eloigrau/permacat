26bc83e6b844d5fea1ff2908efc3c66ef0654bc3
eloigra <eloi.grau@gmail.com>
2022-04-12 21:45:39 +0200

    citealtruiste

--- agoratransition/envoi_mail.py
+++ agoratransition/envoi_mail.py
@@ -1,12 +1,12 @@
 from django.core.mail import get_connection, send_mail
 from django.core.mail.message import EmailMessage
-
+from bourseLibre.settings.production import PERMAGORA_USER_MAIL, PERMAGORA_PWD_MAIL
 
 def envoyerMailPermAgora(sujet, message, destinataires):
     my_host = 'smtp.gmail.com'
     my_port = 587
-    my_username = 'XXXXX'
-    my_password = 'XXXXX'
+    my_username = PERMAGORA_USER_MAIL
+    my_password = PERMAGORA_PWD_MAIL
     my_use_tls = True
     with get_connection(
             host=my_host,
