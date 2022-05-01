902fdb3fd73885054b33f8d0e7ce78621f543f7d
eloigra <eloi.grau@gmail.com>
2022-05-01 22:26:49 +0200

    ²

--- defraiement/admin.py
+++ defraiement/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Reunion, ParticipantReunion
+from .models import Reunion, ParticipantReunion, Distance_ParticipantReunion
 
 # Register your models here.
 
 admin.site.register(ParticipantReunion)
 admin.site.register(Reunion)
+admin.site.register(Distance_ParticipantReunion)
