701de70fb99c110964237f0479215630973d5765
eloigra <eloi.grau@gmail.com>
2022-06-09 23:24:30 +0200

    ²

--- bourseLibre/views_admin.py
+++ bourseLibre/views_admin.py
@@ -224,8 +224,7 @@ def send_mass_html_mail(datatuple, fail_silently=False, auth_user=None,
         fail_silently=fail_silently,
     )
     messages = [
-        EmailMultiAlternatives(subject, message, sender, to=[sender, ],
-                               bcc=recipient,
+        EmailMultiAlternatives(subject, message, sender, to=recipient,
                                alternatives=[(html_message, 'text/html')],
                                connection=connection)
         for subject, message, html_message, sender, recipient in data
