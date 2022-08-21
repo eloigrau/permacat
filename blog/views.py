9fb99cb6d4efefeda0ef090caa197fe338fccc9a
eloigra <eloi.grau@gmail.com>
2022-08-21 23:45:01 +0200

    MAJ v3 asalons articles partagés

--- blog/views.py
+++ blog/views.py
@@ -331,9 +331,9 @@ class ListeArticles_asso(ListView):
     def get_queryset(self):
         params = dict(self.request.GET.items())
         self.asso = Asso.objects.get(abreviation=self.kwargs['asso'])
-        self.q_objects = self.request.user.getQObjectsAssoArticles()
-        qs = Article.objects.filter(self.q_objects).distinct()
-        # qs = Article.objects.filter(Q(asso__abreviation=self.asso.abreviation) | Q(partagesAsso__abreviation=self.asso.abreviation) | Q(partagesAsso__abreviation="public")).distinct()
+        #self.q_objects = self.request.user.getQObjectsAssoArticles()
+        #qs = Article.objects.filter(Q(asso__abreviation=self.asso.abreviation) & self.q_objects).distinct()
+        qs = Article.objects.filter(Q(asso__abreviation=self.asso.abreviation) | Q(partagesAsso__abreviation=self.asso.abreviation) | Q(partagesAsso__abreviation="public")).distinct()
         self.categorie = None
 
         if "auteur" in params:
@@ -357,14 +357,14 @@ class ListeArticles_asso(ListView):
             qs = qs.order_by( '-date_modification', '-date_creation',  'categorie')
 
         self.qs = qs
-        return qs.filter(asso=self.asso, estArchive=False)
+        return [x for x in qs.filter(asso=self.asso, estArchive=False) if x.est_autorise(self.request.user)]
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
 
         context['articles_archives'] = self.qs.filter(estArchive=True)
-        context['articles_epingles'] = self.qs.filter(estEpingle=True, estArchive=False, asso=self.asso)
+        context['articles_epingles'] = self.qs.filter(estEpingle=True, estArchive=False)
         context['articles_partages'] = [x for x in self.qs.filter(~Q(asso=self.asso)) if x.est_autorise(self.request.user)]
 
         # qs = self.qs
